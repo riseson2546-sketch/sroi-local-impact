e31b92740a0692cca75eaf36c1eda4a64a75737f
riseson2546-sketch <riseson2546@gmail.com>
2025-07-29 21:09:35 +0700

    Create SurveyReview.tsx

---
+++ src/pages/SurveyReview.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CompleteSurveyViewer from '../components/admin/CompleteSurveyViewer';
+
+const SurveyReview = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <CompleteSurveyViewer />
+    </div>
+  );
+};
+
+export default SurveyReview;
